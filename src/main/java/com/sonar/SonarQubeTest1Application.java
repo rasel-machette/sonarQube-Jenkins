d33b2761e2191baffa20be23ac2b6bc7d7dee266
rasel <md.rasel@machette.tech>
2021-05-05 20:44:35 +0600

    first commit

---
+++ src/main/java/com/sonar/SonarQubeTest1Application.java
@@ -0,0 +1,13 @@
+package com.sonar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarQubeTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarQubeTest1Application.class, args);
+	}
+
+}
